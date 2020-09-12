b7e2f367e9cf5f7d35ed07a014571db35d192b82
rbdroshni <rbdroshni@gmail.com>
2020-09-12 23:04:08 +0530

    "chart update"

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ProjectManufac
+# TestProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-manufac'`, () => {
+  it(`should have as title 'TestProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-manufac');
+    expect(app.title).toEqual('TestProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('project-manufac app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('TestProject app is running!');
   });
 });
